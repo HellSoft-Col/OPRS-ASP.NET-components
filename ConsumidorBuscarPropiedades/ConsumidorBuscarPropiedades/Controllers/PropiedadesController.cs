302cc8fae86b01e45cadda9991e41c01cb5d2d1f
Andrés Cocunubo <anfecoquin@hotmail.com>
2019-05-30 10:51:15 -0500

    Adicion Proxy

--- ConsumidorBuscarPropiedades/ConsumidorBuscarPropiedades/Controllers/PropiedadesController.cs
+++ ConsumidorBuscarPropiedades/ConsumidorBuscarPropiedades/Controllers/PropiedadesController.cs
@@ -1,18 +1,13 @@
 ﻿using ConsumidorBuscarPropiedades.Models;
-using Newtonsoft.Json;
-using System;
+using ConsumidorBuscarPropiedades.Proxys;
 using System.Collections.Generic;
-using System.Linq;
-using System.Net.Http;
-using System.Net.Http.Headers;
-using System.Web;
 using System.Web.Mvc;
 
 namespace ConsumidorBuscarPropiedades.Controllers
 {
     public class PropiedadesController : Controller
     {
-        string Baseurl = "http://localhost:8080/ServiciosRESTOPRS/webresources/serachProperty";
+        ProxyRESTBuscarPropiedades proxyREST = new ProxyRESTBuscarPropiedades();
         // GET: Propiedades
         public ActionResult Index()
         {
@@ -23,30 +18,10 @@ namespace ConsumidorBuscarPropiedades.Controllers
         [HttpPost]
         public ActionResult Index(string owner_id = "Default", float minPrice = -1, float maxPrice = -1)
         {
-            IEnumerable<Propiedad> propiedad = null;
-
-            using (var client = new HttpClient())
+            IEnumerable<Propiedad> propiedad = proxyREST.ObtenerPropiedades(owner_id,minPrice, maxPrice);
+            if(propiedad != null)
             {
-                PropertyQueryDTO busqueda;
-                busqueda = new PropertyQueryDTO(minPrice, maxPrice, owner_id);
-                var myContent = JsonConvert.SerializeObject(busqueda);
-                var buffer = System.Text.Encoding.UTF8.GetBytes(myContent);
-                var byteContent = new ByteArrayContent(buffer);
-                byteContent.Headers.ContentType = new MediaTypeHeaderValue("application/json");
-                var responseTask = client.PostAsync(Baseurl, byteContent);
-                responseTask.Wait();
-
-                var result = responseTask.Result;
-                if (result.IsSuccessStatusCode)
-                {
-                    var readTask = result.Content.ReadAsStringAsync().Result;
-                    propiedad = JsonConvert.DeserializeObject<List<Propiedad>>(readTask);
-                }
-                else
-                {
-                    propiedad = Enumerable.Empty<Propiedad>();
-                    ModelState.AddModelError(string.Empty, "Server error. Please contact administrator.");
-                }
+                ModelState.AddModelError(string.Empty, "Server error. Please contact administrator.");
             }
             return View(propiedad);
         }
---
+++ ConsumidorBuscarPropiedades/ConsumidorBuscarPropiedades/Proxys/ProxyRESTBuscarPropiedades.cs
@@ -0,0 +1,41 @@
+﻿using ConsumidorBuscarPropiedades.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using Newtonsoft.Json;
+using System.Web.ModelBinding;
+
+namespace ConsumidorBuscarPropiedades.Proxys
+{
+    public class ProxyRESTBuscarPropiedades
+    {
+        private string Baseurl = "http://localhost:8080/ServiciosRESTOPRS/webresources/serachProperty";
+
+        public List<Propiedad> ObtenerPropiedades(string owner_id, float minPrice, float maxPrice)
+        {
+            List<Propiedad> propiedad = null;
+            using (var client = new HttpClient())
+            {
+                PropertyQueryDTO busqueda;
+                busqueda = new PropertyQueryDTO(minPrice, maxPrice, owner_id);
+                var myContent = JsonConvert.SerializeObject(busqueda);
+                var buffer = System.Text.Encoding.UTF8.GetBytes(myContent);
+                var byteContent = new ByteArrayContent(buffer);
+                byteContent.Headers.ContentType = new MediaTypeHeaderValue("application/json");
+                var responseTask = client.PostAsync(Baseurl, byteContent);
+                responseTask.Wait();
+
+                var result = responseTask.Result;
+                if (result.IsSuccessStatusCode)
+                {
+                    var readTask = result.Content.ReadAsStringAsync().Result;
+                    propiedad = JsonConvert.DeserializeObject<List<Propiedad>>(readTask);
+                }
+            }
+            return propiedad;
+        }
+    }
+}
